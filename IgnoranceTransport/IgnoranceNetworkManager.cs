f29f5b125315f16137a89589d1a2aabd7bb6d8fa
Matt Coburn <coburn@dandere.net>
2019-01-09 12:31:05 +1000

    update the sample network manager child class

--- IgnoranceTransport/IgnoranceNetworkManager.cs
+++ IgnoranceTransport/IgnoranceNetworkManager.cs
@@ -4,9 +4,12 @@ using Mirror.Transport;
 
 public class IgnoranceNetworkManager : NetworkManager
 {
+	// The server address to bind to, use this one. You can set it in the Unity Inspector.
+    // Mirror went though some massive rewrites so this is needed.
+	public string m_IgnoranceServerAddress = "127.0.0.1";
     // The server port to bind to, use this one. You can set it in the Unity Inspector.
     // Mirror went though some massive rewrites so this is needed.
-    public ushort m_ServerUDPPort = 7779;
+    public ushort m_IgnoranceServerPort = 7779;
 
     /// <summary>
     /// Initialize the transport for use with Mirror.
@@ -16,7 +19,11 @@ public class IgnoranceNetworkManager : NetworkManager
         // Do not call the following unless you want to use TCP and WebSockets.
         // base.InitializeTransport();
 
-        transport = new IgnoranceTransport(networkAddress, m_ServerUDPPort, (ushort)maxConnections);
+        transport = new IgnoranceTransport(m_IgnoranceServerAddress, m_IgnoranceServerPort, (ushort)maxConnections);
+		
+		// Note that this network manager will be locked to that IP address and port, this is due to NetworkManager getting
+		// some TCP and WebSocket upgrades... So you cannot change the server host address at runtime.
+		
         // Nothing more needs to be done. You're good to go.
     }
 
